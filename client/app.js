65370ec84fba9d6d44730c35aa173d903dbf0b1f
VedantTinkhede <vedanttinkhede797@gmail.com>
2025-06-29 22:41:39 +0530

    UI

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 function getBathValue() {
   var uiBathrooms = document.getElementsByName("uiBathrooms");
-  for(var i = 0; i < uiBathrooms.length; i++) {
+  for(var i in uiBathrooms) {
     if(uiBathrooms[i].checked) {
-        return parseInt(uiBathrooms[i].value);
+        return parseInt(i)+1;
     }
   }
   return -1; // Invalid Value
@@ -10,9 +10,9 @@ function getBathValue() {
 
 function getBHKValue() {
   var uiBHK = document.getElementsByName("uiBHK");
-  for(var i = 0; i < uiBHK.length; i++) {
+  for(var i in uiBHK) {
     if(uiBHK[i].checked) {
-        return parseInt(uiBHK[i].value);
+        return parseInt(i)+1;
     }
   }
   return -1; // Invalid Value
@@ -26,8 +26,8 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-//   var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  var url = "http://127.0.0.1:5000/predict_home_price"; 
+  // var url = "/api/predict_home_price"; 
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -43,8 +43,8 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log( "document loaded" );
-  var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-//   var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  var url = "http://127.0.0.1:5000/get_location_names"; 
+  // var url = "/api/get_location_names"; 
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
